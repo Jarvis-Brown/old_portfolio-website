cc00ae7016a03781dc044f2f9e9db1e03d36279d
Jarvis-Brown <brownjarvis21@gmail.com>
2021-04-23 15:05:43 -0400

    2.3
    need help adding hamburger

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
-
 alert("Hey! Welcome to my portfolio site");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle'
+  }
+}
